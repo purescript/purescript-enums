ebe0f8e8f5ef4844feba847e27283dc53a361311
Gary Burgess <gary.burgess@gmail.com>
2014-04-25 22:25:00 +0100

    Add `succ` and `pred`

--- src/Data/Enum.purs
+++ src/Data/Enum.purs
@@ -5,3 +5,9 @@ import Data.Maybe
 class Enum a where
   toEnum :: Number -> Maybe a
   fromEnum :: a -> Number
+
+succ :: forall a. (Enum a) => a -> Maybe a
+succ x = toEnum (fromEnum x + 1)
+
+pred :: forall a. (Enum a) => a -> Maybe a
+pred x = toEnum (fromEnum x - 1)
