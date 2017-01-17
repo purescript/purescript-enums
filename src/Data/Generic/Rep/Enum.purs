df49831fca947e24cc6487c96dd935e76d8868f1
Matthew Leon <ml@matthewleon.com>
2017-01-17 21:40:09 +0200

    generic: use maybe'
    Use `maybe'` instead of `maybe` for lazy evaluation of the default case.

--- src/Data/Generic/Rep/Enum.purs
+++ src/Data/Generic/Rep/Enum.purs
@@ -7,7 +7,7 @@ module Data.Generic.Rep.Enum
   ) where
 
 import Data.Generic.Rep
-import Data.Maybe (Maybe(..), maybe)
+import Data.Maybe (Maybe(..), maybe')
 import Data.Generic.Rep.Bounded (class GenericBottom, genericBottom',
                                  class GenericTop, genericTop')
 import Prelude ((<$>))
@@ -27,11 +27,11 @@ instance genericEnumConstructor :: GenericEnum (Constructor n NoArguments) where
 instance genericEnumSum ::
   (GenericEnum a, GenericTop a, GenericEnum b, GenericBottom b)
   => GenericEnum (Sum a b) where
-  succ' (Inl x) = Just (maybe (Inr genericBottom') Inl (succ' x))
+  succ' (Inl x) = Just (maybe' (\_ -> Inr genericBottom') Inl (succ' x))
   succ' (Inr x) = Inr <$> succ' x
 
   pred' (Inl x) = Inl <$> pred' x
-  pred' (Inr x) = Just (maybe (Inl genericTop') Inr (pred' x))
+  pred' (Inr x) = Just (maybe' (\_ -> Inl genericTop') Inr (pred' x))
 
 genericSucc :: forall a rep. (Generic a rep, GenericEnum rep)
   => a -> Maybe a
