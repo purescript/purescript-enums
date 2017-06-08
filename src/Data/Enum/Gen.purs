7e61a4c28687fd446506709482f9a29b8b6b349d
Gary Burgess <gary.burgess@gmail.com>
2017-06-08 21:23:16 +0100

    Un-bias the enum generator

--- src/Data/Enum/Gen.purs
+++ src/Data/Enum/Gen.purs
@@ -3,11 +3,16 @@ module Data.Enum.Gen where
 import Prelude
 
 import Control.Monad.Gen (class MonadGen, elements)
-import Data.Enum (class BoundedEnum, enumFromTo)
+import Data.Enum (class BoundedEnum, succ, enumFromTo)
+import Data.Maybe (Maybe(..))
 import Data.NonEmpty ((:|))
 
 -- | Create a random generator for a finite enumeration.
 genBoundedEnum :: forall m a. MonadGen m => BoundedEnum a => m a
 genBoundedEnum =
-  let possibilities = enumFromTo (bottom :: a) (top :: a) :: Array a
-  in elements (bottom :| possibilities)
+  case succ bottom of
+    Just a →
+      let possibilities = enumFromTo a top :: Array a
+      in elements (bottom :| possibilities)
+    Nothing →
+      pure bottom
