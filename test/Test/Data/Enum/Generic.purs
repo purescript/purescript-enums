f5d3c2e40038a3dc840a3e9fd49d760e65c39189
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-24 20:01:29 -0800

    Remove code unrelated to Enum in test file

--- test/Test/Data/Enum/Generic.purs
+++ test/Test/Data/Enum/Generic.purs
@@ -7,30 +7,12 @@ import Data.Generic.Rep as G
 import Data.Generic.Rep.Bounded as GBounded
 import Data.Generic.Rep.Enum as GEnum
 import Data.Generic.Rep.Eq as GEq
-import Data.Generic.Rep.HeytingAlgebra as GHeytingAlgebra
 import Data.Generic.Rep.Ord as GOrd
-import Data.Generic.Rep.Ring as GRing
-import Data.Generic.Rep.Semiring as GSemiring
 import Data.Generic.Rep.Show as GShow
-import Data.HeytingAlgebra (ff, tt)
 import Data.Maybe (Maybe(..))
-import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Console (log, logShow)
 import Test.Assert (assert)
-
-data List a = Nil | Cons { head :: a, tail :: List a }
-
-cons :: forall a. a -> List a -> List a
-cons head tail = Cons { head, tail }
-
-derive instance genericList :: G.Generic (List a) _
-
-instance eqList :: Eq a => Eq (List a) where
-  eq x y = GEq.genericEq x y
-
-instance showList :: Show a => Show (List a) where
-  show x = GShow.genericShow x
 
 data SimpleBounded = A | B | C | D
 derive instance genericSimpleBounded :: G.Generic SimpleBounded _
@@ -108,73 +90,8 @@ instance boundedEnumPair :: (BoundedEnum a, BoundedEnum b) => BoundedEnum (Pair 
   toEnum = GEnum.genericToEnum
   fromEnum = GEnum.genericFromEnum
 
-data A1 = A1 (Tuple (Tuple Int {a :: Int}) {a :: Int})
-derive instance genericA1 :: G.Generic A1 _
-instance eqA1 :: Eq A1 where
-  eq a = GEq.genericEq a
-instance showA1 :: Show A1 where
-  show a = GShow.genericShow a
-instance semiringA1 :: Semiring A1 where
-  zero = GSemiring.genericZero
-  one = GSemiring.genericOne
-  add x y = GSemiring.genericAdd x y
-  mul x y = GSemiring.genericMul x y
-instance ringA1 :: Ring A1 where
-  sub x y = GRing.genericSub x y
-
-data B1 = B1 (Tuple (Tuple Boolean {a :: Boolean}) {a :: Boolean})
-derive instance genericB1 :: G.Generic B1 _
-instance eqB1 :: Eq B1 where
-  eq a = GEq.genericEq a
-instance showB1 :: Show B1 where
-  show a = GShow.genericShow a
-instance heytingAlgebraB1 :: HeytingAlgebra B1 where
-  ff = GHeytingAlgebra.genericFF
-  tt = GHeytingAlgebra.genericTT
-  implies x y = GHeytingAlgebra.genericImplies x y
-  conj x y = GHeytingAlgebra.genericConj x y
-  disj x y = GHeytingAlgebra.genericDisj x y
-  not x = GHeytingAlgebra.genericNot x
-
-instance booleanAlgebraB1 :: BooleanAlgebra B1
-
 main :: Effect Unit
 main = do
-  logShow (cons 1 (cons 2 Nil))
-
-  log "Checking equality"
-  assert $ cons 1 (cons 2 Nil) == cons 1 (cons 2 Nil)
-
-  log "Checking inequality"
-  assert $ cons 1 (cons 2 Nil) /= cons 1 Nil
-
-  log "Checking comparison EQ"
-  assert $ (Pair Zero (Some One) `compare` Pair Zero (Some One)) == EQ
-
-  log "Checking comparison GT"
-  assert $ (Pair (Some One) Zero `compare` Pair (Some Zero) Zero) == GT
-
-  log "Checking comparison LT"
-  assert $ (Pair Zero One `compare` Pair One One) == LT
-
-  log "Checking simple bottom"
-  assert $ bottom == A
-
-  log "Checking simple top"
-  assert $ top == D
-
-  log "Checking composite bottom"
-  assert $ bottom == (None :: Option SimpleBounded)
-
-  log "Checking composite top"
-  assert $ top == Some D
-
-  log "Checking product bottom"
-  assert $ bottom == (Pair Zero A :: Pair Bit SimpleBounded)
-
-  log "Checking product top"
-  assert $ top == (Pair One D :: Pair Bit SimpleBounded)
-
   log "Checking simple pred bottom"
   assert $ pred (bottom :: SimpleBounded) == Nothing
 
@@ -231,33 +148,3 @@ main = do
   log "Checking product toEnum/fromEnum roundtrip"
   assert $ let allPairs = enumFromTo bottom top :: Array (Pair Bit SimpleBounded)
            in (toEnum <<< fromEnum <$> allPairs) == (Just <$> allPairs)
-
-  log "Checking zero"
-  assert $ (zero :: A1) == A1 (Tuple (Tuple 0 {a: 0}) {a: 0})
-
-  log "Checking one"
-  assert $ (one :: A1) == A1 (Tuple (Tuple 1 {a: 1}) {a: 1})
-
-  log "Checking add"
-  assert $ A1 (Tuple (Tuple 100 {a: 10}) {a: 20}) + A1 (Tuple (Tuple 50 {a: 30}) {a: 40}) == A1 (Tuple (Tuple 150 {a: 40}) {a: 60})
-
-  log "Checking mul"
-  assert $ A1 (Tuple (Tuple 100 {a: 10}) {a: 20}) * A1 (Tuple (Tuple 50 {a: 30}) {a: 40}) == A1 (Tuple (Tuple 5000 {a: 300}) {a: 800})
-
-  log "Checking sub"
-  assert $ A1 (Tuple (Tuple 100 {a: 10}) {a: 20}) - A1 (Tuple (Tuple 50 {a: 30}) {a: 40}) == A1 (Tuple (Tuple 50 {a: -20}) {a: -20})
-
-  log "Checking ff"
-  assert $ (ff :: B1) == B1 (Tuple (Tuple false {a: false}) {a: false})
-
-  log "Checking tt"
-  assert $ (tt :: B1) == B1 (Tuple (Tuple true {a: true}) {a: true})
-
-  log "Checking conj"
-  assert $ (B1 (Tuple (Tuple true {a: false}) {a: true}) && B1 (Tuple (Tuple false {a: false}) {a: true})) == B1 (Tuple (Tuple false { a: false }) { a: true })
-
-  log "Checking disj"
-  assert $ (B1 (Tuple (Tuple true {a: false}) {a: true}) || B1 (Tuple (Tuple false {a: false}) {a: true})) == B1 (Tuple (Tuple true { a: false }) { a: true })
-
-  log "Checking not"
-  assert $ not B1 (Tuple (Tuple true {a: false}) {a: true}) == B1 (Tuple (Tuple false {a: true}) {a: false})
